c343fd89fac1996812c1735b59ff90472a83aaf5
dschien <dschien@gmail.com>
2018-01-18 10:58:30 +0000

    can process assignments

--- dsl_listener.py
+++ dsl_listener.py
@@ -4,7 +4,7 @@ import antlr4
 
 from arithmeticLexer import arithmeticLexer
 from arithmeticListener import arithmeticListener
-from arithmeticParser import arithmeticParser, ParseTreeWalker, TerminalNode
+from arithmeticParser import arithmeticParser, ParseTreeWalker, TerminalNode, ParserRuleContext
 
 
 class DSL(arithmeticListener):
@@ -14,6 +14,11 @@ class DSL(arithmeticListener):
         self.result = None
         self.parseTreeProperties = {}
         self.variables = variables if variables else {}
+        self.isParsingAssignmentCtx = False
+
+    # def enterEquation(self, ctx: arithmeticParser.EquationContext):
+    #     if isinstance(ctx.getChild(1), arithmeticParser.EQ):
+    #         self.isParsingAssignmentCtx = True
 
     def exitEquation(self, ctx: arithmeticParser.EquationContext):
 
@@ -24,25 +29,47 @@ class DSL(arithmeticListener):
             print(var_name)
             self.variables[var_name] = self.parseTreeProperties[ctx.expression(1)]
 
+    def enterExpression(self, ctx: arithmeticParser.ExpressionContext):
+        """
+        We need to know if we are in the left hand side context of an assignment.
+
+        :param ctx:
+        :return:
+        """
+        # For simplicity we assume that such a LH expression context will always be
+        # a direct child of an equation.
+        if type(ctx.parentCtx) == arithmeticParser.EquationContext:
+            if ctx == ctx.parentCtx.getChild(0):
+                self.isParsingAssignmentCtx = True
+
     def exitExpression(self, ctx: arithmeticParser.ExpressionContext):
+        if self.isParsingAssignmentCtx:
+            self.isParsingAssignmentCtx = False
+            return
         if ctx.getChildCount() == 1:
             self.parseTreeProperties[ctx] = self.parseTreeProperties[ctx.getChild(0)]
         else:
             raise NotImplementedError()
 
     def exitTerm(self, ctx: arithmeticParser.TermContext):
+        if self.isParsingAssignmentCtx:
+            return
         if ctx.getChildCount() == 1:
             self.parseTreeProperties[ctx] = self.parseTreeProperties[ctx.getChild(0)]
         else:
             raise NotImplementedError()
 
     def exitFactor(self, ctx: arithmeticParser.FactorContext):
+        if self.isParsingAssignmentCtx:
+            return
         if ctx.getChildCount() == 1:
             self.parseTreeProperties[ctx] = self.parseTreeProperties[ctx.getChild(0)]
         else:
             raise NotImplementedError()
 
     def exitSignedAtom(self, ctx: arithmeticParser.SignedAtomContext):
+        if self.isParsingAssignmentCtx:
+            return
         if ctx.getChildCount() == 1:
             self.parseTreeProperties[ctx] = self.parseTreeProperties[ctx.getChild(0)]
         else:
@@ -52,13 +79,24 @@ class DSL(arithmeticListener):
         self.parseTreeProperties[ctx] = int(ctx.getText())
 
     def exitAtom(self, ctx: arithmeticParser.AtomContext):
+        if self.isParsingAssignmentCtx:
+            return
         if ctx.getChildCount() == 1:
             self.parseTreeProperties[ctx] = self.parseTreeProperties[ctx.getChild(0)]
         else:
             raise NotImplementedError()
 
     def enterVariable(self, ctx: arithmeticParser.VariableContext):
+        if self.isParsingAssignmentCtx:
+            return
+
         self.parseTreeProperties[ctx] = self.variables[ctx.getText()]
+
+    @staticmethod
+    def get_progenitor(ctx: ParserRuleContext):
+        while ctx.parentCtx:
+            ctx = ctx.parentCtx
+        return ctx
 
 
 def main():
